4563c300e625d7786e2a0b39c31c4a38c70146b7
sanjeevveerapandian <115895064+sanjeevveerapandian@users.noreply.github.com>
2024-12-22 22:57:03 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://sanjeevveerapandian:sanjeev004@bookstore-mern.ekjar.mongodb.net/?retryWrites=true&w=majority&appName=BookStore-Mern";
+
+//
